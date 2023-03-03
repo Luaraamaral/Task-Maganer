7d46ee6beee3f36ef00d295a481e26c3d8ffbedc
Luara Amaral <98507687+Luaraamaral@users.noreply.github.com>
2023-03-03 20:30:03 -0300

    Implementing the task class

--- src/main/java/br/com/lacompany/taskmanager/domain/Task.java
+++ src/main/java/br/com/lacompany/taskmanager/domain/Task.java
@@ -1,9 +1,26 @@
 package br.com.lacompany.taskmanager.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tasks")
+@Data
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+
+    public Task(String title, String description, LocalDate dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
 }
