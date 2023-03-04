5c63343d18b5aca13ddc12613f4b23b9fd0753a1
Luara Amaral <98507687+Luaraamaral@users.noreply.github.com>
2023-03-03 22:25:26 -0300

    Changes: Controller, domain and application.properties; creation: exception and service; inclusion: JS, jQuery and CSS.

--- src/main/java/br/com/lacompany/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/lacompany/taskmanager/controller/TaskController.java
@@ -1,32 +1,11 @@
 package br.com.lacompany.taskmanager.controller;
 
-import br.com.lacompany.taskmanager.domain.Task;
-import br.com.lacompany.taskmanager.service.TaskService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
-    @Autowired
-    private TaskService taskService;
 
-    @GetMapping("/findAllTasks")
-    public ResponseEntity<List<Task>> findAllTasks() {
-        List<Task> tasks = taskService.findAll();
-        return ResponseEntity.ok(tasks);
-    }
-
-    @PostMapping("/createTask")
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        Task newTask = taskService.createTask(task);
-        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
-    }
 
 }
--- src/main/java/br/com/lacompany/taskmanager/domain/Task.java
+++ src/main/java/br/com/lacompany/taskmanager/domain/Task.java
@@ -1,32 +1,26 @@
 package br.com.lacompany.taskmanager.domain;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tasks")
-@NoArgsConstructor
 @Data
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
-    @Column(name = "title")
     private String title;
-    @Column(name = "description")
     private String description;
-
-    @Column(name = "dueDate")
     private LocalDate dueDate;
 
-    public Task(String title, String description) {
+    public Task(String title, String description, LocalDate dueDate) {
         this.title = title;
         this.description = description;
+        this.dueDate = dueDate;
     }
 
 }
